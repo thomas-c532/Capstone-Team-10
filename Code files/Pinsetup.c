44ff714c993a3cb12164515a1772eaf461e2a8f8
thomas-c532 <thomaschapman532@gmail.com>
2022-12-28 01:27:20 -0600

    Final structure for start of project

--- Code files/Pinsetup.c
+++ Code files/Pinsetup.c
@@ -1,17 +1,7 @@
 #include "stm32f4xx.h"
 
 void PinSetup(void){
-RCC->AHB2ENR |= 0x01; /*enable GPIOA clock*/
-GPIOA->MODER &= ~(0x00FF0FFF);
-GPIOA->MODER |= 0x00550002; 
-GPIOA->MODER &= 0xFFF3;
-GPIOA->MODER |= 0x000C;
-GPIOA->PUPDR &= ~(0x00000FF0); 
-GPIOA->PUPDR |= 0x00000550;
-RCC->AHB2ENR |= 0x02; /*enable GPIOB clock*/
-GPIOB->MODER &= 0xFFFF0000; 
-GPIOB->MODER |= 0x00001540;
-GPIOA->AFR[0] &= 0xFFFFFFF0;
-GPIOA->AFR[0] |= 0x00000001;
-//This is a test to see if pulling the entire project will work
+
+
+	
 }
