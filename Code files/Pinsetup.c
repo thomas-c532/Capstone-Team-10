d58246f1fa15af43c7df3cc80b4a6de47e349be7
thomas-c532 <thomaschapman532@gmail.com>
2023-01-03 12:51:08 -0600

    Update Pinsetup.c

--- Code files/Pinsetup.c
+++ Code files/Pinsetup.c
@@ -1,17 +1,16 @@
 #include "stm32f4xx.h"
 
 void PinSetup(void){
-RCC->AHB2ENR |= 0x01; /*enable GPIOA clock*/
-GPIOA->MODER &= ~(0x00FF0FFF);
-GPIOA->MODER |= 0x00550002; 
-GPIOA->MODER &= 0xFFF3;
-GPIOA->MODER |= 0x000C;
-GPIOA->PUPDR &= ~(0x00000FF0); 
-GPIOA->PUPDR |= 0x00000550;
-RCC->AHB2ENR |= 0x02; /*enable GPIOB clock*/
-GPIOB->MODER &= 0xFFFF0000; 
-GPIOB->MODER |= 0x00001540;
-GPIOA->AFR[0] &= 0xFFFFFFF0;
-GPIOA->AFR[0] |= 0x00000001;
-//This is a test to see if pulling the entire project will work
+		RCC->AHB2ENR |= 0x01; 
+		GPIOA->MODER  &= 0x00000001;
+		RCC->APB1ENR |= (0x0000001);						 /*enables TIM2 from Reset and Clock Control Register*/
+	TIM2->CR1 |= (0x0001); 									/* CR1 sets the configuration for the counter. Currently at default, except for the enabling bit*/
+	TIM2->CCMR1 &= (0xFFFFFF80); 					  /*Clears and sets necissary bits for capture/control mode reg. for PWM mode 1.*/
+	TIM2->CCMR1 |= (0x00000060);  					 /*Sets hex LSB to zero to configure to output mode*/
+	TIM2->CCER &= (0xFFF0);
+	TIM2->CCER |= (0x0001); 								/*Enables capture/compare output to be active high, and to activate the output*/
+	TIM2->ARR = (0x0236);
+	TIM2->CCR1 = (0x0238*0.5);
+	NVIC_EnableIRQ(TIM2_IRQn); 
+	NVIC_ClearPendingIRQ(TIM2_IRQn);
 }
