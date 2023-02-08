e72dd188f2b46753ae296ab784a6dc359185b61d
thomas-c532 <121457850+thomas-c532@users.noreply.github.com>
2023-02-08 10:28:00 -0600

    Update PINSETUP.c
    Fixed the flex sensor inputs to be analog for ADCs instead of input pins

--- Code files/PINSETUP.c
+++ Code files/PINSETUP.c
@@ -5,13 +5,13 @@ void PinSetup(void){
 		RCC->AHB2ENR |= 0x00000007; 			/*Enable the clock for I/O ports A,B, and C*/
 	
 		/*PINS ON CN10 BUS*/
-		GPIOB->MODER &= 0x00FFFFC3;			/*Setting PB1,2,13,14, and 15 to input for Finger Sensors. Also clears PB12 for next instruction*/
-		GPIOB->MODER |= 0x01000000;			/*Sets PB12 to output*/
-		GPIOC->MODER &= 0xFFFFFCFF;     		/*Setting PC4 to input for wrist sensor*/
-	
-		/*PINS ON CN5 BUS*/
-		GPIOA->MODER &= 0xFFCFFF0F;     		/*Clearing bits for PA2,3, and 10*/
+		GPIOB->MODER &= 0x00FFFFC3;			/*Clearing PB1,2,13,14, and 15. Also clears PB12*/
+		GPIOB->MODER |= 0xFD00003C;			/*Sets PB12 to output, and PB1,2,13,14,15 to analog*/
+		GPIOC->MODER &= 0xFFFFFCFF;			/*Clears PC4*/
+		GPIOC->MODER |= 0x00000300;     /*Setting PC4 to analog for wrist sensor*/
+		GPIOA->MODER &= 0xFFCFFF0F;     /*Clearing bits for PA2,3, and 10*/
 		GPIOA->MODER |= 0x00100050;			/*Setting PA2,3, and 10 to outputs*/
+
 		
 		/*PINS ON CN6 BUS*/
 
