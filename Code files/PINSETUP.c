74c00d5e29a09da993f5c97f6d41d81991ebaffb
gnat1hand1yall <122483525+gnat1hand1yall@users.noreply.github.com>
2023-02-08 14:33:29 -0600

    Update PINSETUP.c

--- Code files/PINSETUP.c
+++ Code files/PINSETUP.c
@@ -1,25 +1,26 @@
 #include "stm32f4xx.h"
 
-void PinSetup(void){
-
-		RCC->AHB2ENR |= 0x00000007; 			/*Enable the clock for I/O ports A,B, and C*/
+void PinSetup(void)
+{
+	RCC->AHB1ENR |= 0x00000007; 			/*Enable the clock for I/O ports A,B, and C*/
 	
-		/*PINS ON CN10 BUS*/
-		GPIOB->MODER &= 0x00FFFFC3;			/*Setting PB1,2,13,14, and 15 to input for Finger Sensors. Also clears PB12 for next instruction*/
-		GPIOB->MODER |= 0x01000000;			/*Sets PB12 to output*/
-		GPIOC->MODER &= 0xFFFFFCFF;     		/*Setting PC4 to input for wrist sensor*/
-	
-		/*PINS ON CN5 BUS*/
-		GPIOA->MODER &= 0xFFCFFF0F;     		/*Clearing bits for PA2,3, and 10*/
-		GPIOA->MODER |= 0x00100050;			/*Setting PA2,3, and 10 to outputs*/
-		
-		/*PINS ON CN6 BUS*/
+	/*PINS ON CN10 BUS*/
+	GPIOA->MODER &= 0xFFCFFF0F;     		/*Clearing bits for PA2, 3, and 10; xxxx xxxx xx00 xxxx xxxx xxxx 0000 xxxx*/
+	GPIOA->MODER |= 0x00100050;			/*Setting PA2, 3, and 10 to outputs; xxxx xxxx xx01 xxxx xxxx xxxx 0101 xxxx*/
+	GPIOB->MODER &= 0x00FFFFC3;			/*Clearing PB1, 2, 13, 14, and 15. Also clears PB12; 0000 0000 xxxx xxxx xxxx xxxx xx00 00xx*/
+	GPIOB->MODER |= 0xFD00003C;			/*Sets PB12 to output, and PB1, 2, 13, 14, 15 to analog; 1111 1101 xxxx xxxx xxxx xxxx xx11 11xx*/
+	GPIOC->MODER &= 0xFFFFFCFF;			/*Clears PC4; xxxx xxxx xxxx xxxx xxxx xx00 xxxx xxxx*/
+	GPIOC->MODER |= 0x00000300;     		/*Setting PC4 to analog for wrist sensor; xxxx xxxx xxxx xxxx xxxx xx11 xxxx xxxx*/
 
-		/*PINS ON CN7 BUS*/
-		GPIOC->MODER &= 0x03FFFF0F;			/*Clear bits for PC 2,3,13,14, and 15*/
-		GPIOC->MODER |= 0x54000050;			/*Setting PC 2,3,13,14,15 to outputs (PWMs for motor drivers)*/
-		GPIOB->MODER &= 0xFFFFFFFF;			/*Clearing PB7*/
-		GPIOB->MODER |= 0x00004000;			/*Setting PB7 to output (for wrist joint motor driver)*/
+	/*PINS ON CN6 BUS*/
+
+	/*PINS ON CN7 BUS*/
+	GPIOB->MODER &= 0xFFFF3FFF;			/*Clearing PB7; xxxx xxxx xxxx xxxx 00xx xxxx xxxx xxxx*/
+	GPIOB->MODER |= 0x00004000;			/*Setting PB7 to output (for wrist joint motor driver); xxxx xxxx xxxx xxxx 01xx xxxx xxxx xxxx*/
+	GPIOC->MODER &= 0x03FFFF0F;			/*Clear bits for PC2, 3, 13, 14, and 15; 0000 00xx xxxx xxxx xxxx xxxx 0000 xxxx*/
+	GPIOC->MODER |= 0x54000050;			/*Setting PC2, 3, 13, 14, 15 to outputs (PWMs for motor drivers); 0101 01xx xxxx xxxx xxxx xxxx 0101 xxxx*/
 }
+
+		
 
 		
