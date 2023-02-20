287f68dd37146a1622c1ec41dd4290429c30be6d
thomas-c532 <121457850+thomas-c532@users.noreply.github.com>
2023-02-20 12:44:37 -0600

    Preliminary setup of DMA, adjusting main

--- Code files/Main.c
+++ Code files/Main.c
@@ -3,8 +3,13 @@
 #include "Setupfunctions.h"
 #include "stm32f4xx.h"
 int main(void) {
- 
-	//Hello world!
+	PINSETUP();
+	ADCSETUP();
+	TIMERSETUP();
+	DMASETUP();
 	
+	/*DMA streams are set in circular mode, so they should continously update. The only thing that should have to be put in 
+	the main code are the conversion factors of ADC input to a usuable value, and then storing those values in a memory lcoation.
+	That memory location will be set in the DMASETUP.c and will start to stream data*/
   return 0;
 }
